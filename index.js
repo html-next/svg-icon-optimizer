6563ebdecd680d322e5e260057f0e7f52f385fb5
Chris Thoburn <runspired@users.noreply.github.com>
2022-04-06 09:37:39 -0700

    fix formatting

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 'use strict';
 
-const Funnel = require("broccoli-funnel");
-const merge = require("broccoli-merge-trees");
-const path = require("path");
+const Funnel = require('broccoli-funnel');
+const merge = require('broccoli-merge-trees');
+const path = require('path');
 
 function dasherize(str) {
   return str.replace(/[A-Z]/g, function (char, index) {
-    return (index !== 0 ? "-" : "") + char.toLowerCase();
+    return (index !== 0 ? '-' : '') + char.toLowerCase();
   });
 }
 
 function templatePrecompiler(env, usedIcons) {
   const b = env.syntax.builders;
   return {
-    name: "svg-component-rewrite",
+    name: 'svg-component-rewrite',
     visitor: {
       ElementNode(node) {
-        if (node.tag.startsWith("Icon::")) {
+        if (node.tag.startsWith('Icon::')) {
           const tag = node.tag;
-          const parts = node.tag.split("::");
+          const parts = node.tag.split('::');
           parts.shift();
-          const fullName = dasherize(parts.join("/"));
+          const fullName = dasherize(parts.join('/'));
           usedIcons.set(fullName, tag);
-          node.tag = "SVGIcon";
-          const attr = b.attr("@name", b.text(fullName));
+          node.tag = 'SVGIcon';
+          const attr = b.attr('@name', b.text(fullName));
           node.attributes.push(attr);
         }
       },
@@ -39,28 +39,34 @@ module.exports = {
   },
 
   pathForMainFilesRoot() {
-    const projectPath = this.parent.root || require.resolve(this.parent.options.name);
+    const projectPath =
+      this.parent.root || require.resolve(this.parent.options.name);
 
-    return path.join(projectPath, this.parentIsAddon() ? "addon" : "app");
+    return path.join(projectPath, this.parentIsAddon() ? 'addon' : 'app');
   },
 
   preprocessTree(type, tree) {
-    if (type === "template") {
+    if (type === 'template') {
       return new Funnel(tree, {
-        exclude: ["components/icon/**"],
+        exclude: ['components/icon/**'],
       });
     }
     return tree;
   },
 
   treeForPublic(tree) {
-    const treeForIcons = new Funnel(path.join(this.pathForMainFilesRoot(), "./components/icon"), {
-      destDir: "assets/component-icons",
-      getDestinationPath(relativePath) {
-        return relativePath.replace(".hbs", ".svg");
-      },
-    });
-    return tree ? merge([tree, treeForIcons], { overwrite: true }) : treeForIcons;
+    const treeForIcons = new Funnel(
+      path.join(this.pathForMainFilesRoot(), './components/icon'),
+      {
+        destDir: 'assets/component-icons',
+        getDestinationPath(relativePath) {
+          return relativePath.replace('.hbs', '.svg');
+        },
+      }
+    );
+    return tree
+      ? merge([tree, treeForIcons], { overwrite: true })
+      : treeForIcons;
   },
 
   setupPreprocessorRegistry(type, registry) {
@@ -69,10 +75,10 @@ module.exports = {
     // TODO make this something in the file system
     this.usedIcons = usedIcons;
 
-    if (type === "parent") {
-      registry.add("htmlbars-ast-plugin", {
-        name: "svg-component-rewrite",
-        ext: "hbs",
+    if (type === 'parent') {
+      registry.add('htmlbars-ast-plugin', {
+        name: 'svg-component-rewrite',
+        ext: 'hbs',
         plugin: (env) => {
           // TODO write config to file so that this can be safely parallelized
           return templatePrecompiler(env, usedIcons);
