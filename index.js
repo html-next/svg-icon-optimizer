5e44a1f3f0e7784cf7f58075c962217fb0f604f0
Chris Thoburn <runspired@users.noreply.github.com>
2022-04-07 04:42:51 -0700

    get it mostly humming

--- index.js
+++ index.js
@@ -2,26 +2,22 @@
 
 const Funnel = require('broccoli-funnel');
 const merge = require('broccoli-merge-trees');
+const SVGOptimizer = require('./lib/svgo-optimizer');
 const path = require('path');
+const SpriteConfigGenerator = require('./lib/sprite-config-generator');
+const SpriteAssembler = require('./lib/sprite-assembler');
+const dasherize = require('./lib/utils/dasherize');
 
-function dasherize(str) {
-  return str.replace(/[A-Z]/g, function (char, index) {
-    return (index !== 0 ? '-' : '') + char.toLowerCase();
-  });
-}
-
-function templatePrecompiler(env, usedIcons) {
+function templatePrecompiler(env) {
   const b = env.syntax.builders;
   return {
     name: 'svg-component-rewrite',
     visitor: {
       ElementNode(node) {
         if (node.tag.startsWith('Icon::')) {
-          const tag = node.tag;
           const parts = node.tag.split('::');
           parts.shift();
           const fullName = parts.map(dasherize).join('/');
-          usedIcons.set(fullName, tag);
           node.tag = 'SVGIcon';
           const attr = b.attr('@name', b.text(fullName));
           node.attributes.push(attr);
@@ -64,25 +60,55 @@ module.exports = {
         },
       }
     );
+    const treeForTemplates = new Funnel(this.pathForMainFilesRoot(), {
+      include: ['**/*.hbs'],
+      exclude: ['components/icon/**/*.hbs'],
+      getDestinationPath(relativePath) {
+        return relativePath.replace('.hbs', '.svg');
+      },
+    });
+    const configTree = new SpriteConfigGenerator(treeForTemplates);
+
+    const TrulyRemoveComments = {
+      name: 'Remove Comments Starting With !',
+      type: 'visitor',
+      fn: () => {
+        return {
+          comment: {
+            enter: (node, parentNode) => {
+              if (node.value.charAt(0) === '!') {
+                parentNode.children = parentNode.children.filter(
+                  (child) => child !== node
+                );
+              }
+            },
+          },
+        };
+      },
+    };
+
+    const compiled = new SVGOptimizer(treeForIcons, {
+      persist: false,
+      svgoConfig: {
+        multipass: true,
+        plugins: ['preset-default', TrulyRemoveComments],
+      },
+    });
+    const spriteTree = new SpriteAssembler([configTree, compiled]);
     return tree
-      ? merge([tree, treeForIcons], { overwrite: true })
-      : treeForIcons;
+      ? merge([tree, spriteTree, compiled], { overwrite: true })
+      : merge([spriteTree, compiled], { overwrite: true });
   },
 
   setupPreprocessorRegistry(type, registry) {
-    const usedIcons = new Map();
-    // TODO make this something in the file system
-    this.usedIcons = usedIcons;
-
     if (type === 'parent') {
       registry.add('htmlbars-ast-plugin', {
         name: 'svg-component-rewrite',
         ext: 'hbs',
-        plugin: (env) => {
-          // TODO write config to file so that this can be safely parallelized
-          return templatePrecompiler(env, usedIcons);
+        plugin: templatePrecompiler,
+        cacheKey: () => {
+          return `${Date.now()}`; // use this to deactivate when deving
         },
-        // cacheKey: () => {},
         baseDir() {
           return __dirname;
         },
